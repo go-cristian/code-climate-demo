e700036074d1c18a364d6549b6e1edee325149d9
Cristian Gomez <cristianfgr@gmail.co>
2017-07-24 18:40:31 -0500

    feat($CI): Failed style
    Provided formatter style issues
    
    Styles

--- app/src/main/java/com/iyubinest/codeclimatedemo/MainActivity.java
+++ app/src/main/java/com/iyubinest/codeclimatedemo/MainActivity.java
@@ -2,10 +2,10 @@ package com.iyubinest.codeclimatedemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+  public class MainActivity extends AppCompatActivity {
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);setContentView(R.layout.activity_main);
   }
 }
